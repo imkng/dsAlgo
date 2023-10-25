c3ff6ba8ffaeaf5a9376569cd5e738859aa3cdfc
krnand <1india.kng@gmail.com>
2023-10-25 16:07:22 +0530

    updated Count Negative Numbers In A SortedMatrix solution

--- src/com/company/BinarySearch/CountNegativeNumbersInASortedMatrix.java
+++ src/com/company/BinarySearch/CountNegativeNumbersInASortedMatrix.java
@@ -17,24 +17,23 @@ public class CountNegativeNumbersInASortedMatrix {
 
     private static int countNegatives(int[][] grid) {
         int count = 0;
-        for (int i = 0; i < grid.length; i++) {
+        for (int[] ints : grid) {
             int start = 0;
-            int end = grid[i].length - 1;
-            int absFloor = 0;
+            int end = ints.length - 1;
             int floor = -1;
             while (start <= end) {
                 int mid = start + (end - start) / 2;
-                if (grid[i][mid] == 0) {
+                if (ints[mid] == 0) {
                     start = mid + 1;
-                } else if (grid[i][mid] > 0) {
+                } else if (ints[mid] > 0) {
                     start = mid + 1;
-                } else if (grid[i][mid] < 0) {
+                } else if (ints[mid] < 0) {
                     floor = mid;
                     end = mid - 1;
                 }
             }
             if (floor != -1) {
-                count += grid[i].length - floor;
+                count += ints.length - floor;
             }
         }
         return count;
