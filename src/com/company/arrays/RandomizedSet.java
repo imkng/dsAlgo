500608f22d536e934f73ba4fa6667b115d5233cd
krnand <1india.kng@gmail.com>
2024-01-17 11:44:44 +0530

    format

--- src/com/company/arrays/RandomizedSet.java
+++ src/com/company/arrays/RandomizedSet.java
@@ -14,14 +14,15 @@ public class RandomizedSet {
     }
 
     public boolean insert(int val) {
-        if(list.contains(val)){
-            return false;}
+        if (list.contains(val)) {
+            return false;
+        }
         list.add(val);
         return true;
     }
 
     public boolean remove(int val) {
-        if(!list.contains(val))
+        if (!list.contains(val))
             return false;
         list.remove(Integer.valueOf(val));
         return true;
@@ -33,13 +34,13 @@ public class RandomizedSet {
 
     public static void main(String[] args) {
         RandomizedSet randomizedSet = new RandomizedSet();
-        System.out.println("insert: " + 1 + " " +randomizedSet.insert(1));
+        System.out.println("insert: " + 1 + " " + randomizedSet.insert(1));
         System.out.println("remove: " + 2 + " " + randomizedSet.remove(2));
         System.out.println("insert: " + 2 + " " + randomizedSet.insert(2));
         System.out.println("genRandom: " + " " + randomizedSet.getRandom());
         System.out.println("remove: " + 1 + " " + randomizedSet.remove(1));
-        System.out.println("insert: " + 2 + " " +  randomizedSet.insert(2));
-        System.out.println("insert: " + 2 + " " +  randomizedSet.insert(2));
+        System.out.println("insert: " + 2 + " " + randomizedSet.insert(2));
+        System.out.println("insert: " + 2 + " " + randomizedSet.insert(2));
         System.out.println("getRandom: " + randomizedSet.getRandom());
     }
 }
