4ed8bf0087aa60caf6c150982bf30b9228a1bcc0
krnand <1india.kng@gmail.com>
2024-04-05 11:16:00 +0530

    1544. Make The String Great solution

--- src/com/company/arrays/MakeTheStringGreat.java
+++ src/com/company/arrays/MakeTheStringGreat.java
@@ -43,13 +43,13 @@ public class MakeTheStringGreat {
         Stack<Character> stack = new Stack<>();
         StringBuilder stringBuilder = new StringBuilder("");
         for (int i = 0; i < s.length(); i++) {
-            if( !stack.isEmpty() && Math.abs(stack.peek() - s.charAt(i)) == 32){
+            if (!stack.isEmpty() && Math.abs(stack.peek() - s.charAt(i)) == 32) {
                 stack.pop();
-            }else {
+            } else {
                 stack.push(s.charAt(i));
             }
         }
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             stringBuilder.append(stack.pop());
         }
         stringBuilder.reverse();
