b0f30c9b8b513ea89f5e40b68c1c727e2a54af06
Krishna Nand <1india.kng@gmail.com>
2025-09-11 23:45:38 +0530

    SortVowelsInAString

---
+++ src/com/company/leetCode/SortVowelsInAString.java
@@ -0,0 +1,81 @@
+package com.company.leetCode;
+
+import java.util.PriorityQueue;
+
+class VowelPair{
+    int ascii;
+    char vowel;
+
+    public VowelPair(int ascii, char vowel) {
+        this.ascii = ascii;
+        this.vowel = vowel;
+    }
+
+    public int getAscii() {
+        return ascii;
+    }
+
+    public char getVowel() {
+        return vowel;
+    }
+}
+public class SortVowelsInAString {
+//    Given a 0-indexed string s, permute s to get a new string t such that:
+//
+//    All consonants remain in their original places. More formally, if there is an
+//    index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+//    The vowels must be sorted in the nondecreasing order of their ASCII values. More formally,
+//    for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels,
+//    then t[i] must not have a higher ASCII value than t[j].
+//    Return the resulting string.
+//
+//    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase.
+//    Consonants comprise all letters that are not vowels.
+//
+//
+//
+//    Example 1:
+//
+//    Input: s = "lEetcOde"
+//    Output: "lEOtcede"
+//    Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants.
+//    The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+//    Example 2:
+//
+//    Input: s = "lYmpH"
+//    Output: "lYmpH"
+//    Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+    public static void main(String[] args) {
+        String s = "lEetcOde";
+        String ans = sortVowels(s);
+        System.out.println(ans);
+    }
+
+    private static String sortVowels(String s) {
+        PriorityQueue<VowelPair> maxPq = new PriorityQueue<>((a, b)->a.getAscii() - b.getAscii());
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        for (int i = 0; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))){
+                maxPq.add(new VowelPair(s.charAt(i),s.charAt(i)));
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!isVowel(s.charAt(i))){
+                stringBuilder.append(s.charAt(i));
+            }else{
+                stringBuilder.append(maxPq.poll().getVowel());
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static boolean isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            return true;
+        }
+        return false;
+    }
+}
