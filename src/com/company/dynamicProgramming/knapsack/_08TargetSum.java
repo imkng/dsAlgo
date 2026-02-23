04fe5f1b60d5f5c9e0278ca91b35946337c2a0d9
Krishna Nand <1india.kng@gmail.com>
2026-02-23 22:40:48 +0530

    _08TargetSum

---
+++ src/com/company/dynamicProgramming/knapsack/_08TargetSum.java
@@ -0,0 +1,71 @@
+package com.company.dynamicProgramming.knapsack;
+
+public class _08TargetSum {
+//    You are given an integer array nums and an integer target.
+//
+//    You want to build an expression out of nums by adding one of the symbols '+' and '-' before
+//    each integer in nums and then concatenate all the integers.
+//
+//    For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate
+//    them to build the expression "+2-1".
+//    Return the number of different expressions that you can build, which evaluates to target.
+
+//    Example 1:
+//
+//    Input: nums = [1,1,1,1,1], target = 3
+//    Output: 5
+//    Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+//            -1 + 1 + 1 + 1 + 1 = 3 ===> (1+1+1+1) - 1 = 3
+//            +1 - 1 + 1 + 1 + 1 = 3
+//            +1 + 1 - 1 + 1 + 1 = 3
+//            +1 + 1 + 1 - 1 + 1 = 3
+//            +1 + 1 + 1 + 1 - 1 = 3
+//    Example 2:
+//
+//    Input: nums = [1], target = 1
+//    Output: 1
+    public static void main(String[] args) {
+        // s1 - s2 = diff;
+        // s1  + s2 = range;
+        // 2s1 = diff + range
+        // s1 = (diff + range)/2 => so, count number of subsetSum s1
+
+        int [] nums = {1,1,1,1,1};
+        int target = 3;
+
+        int ans = targetSum(nums, target);
+        System.out.println(ans);
+
+    }
+
+    private static int targetSum(int[] nums, int target) {
+        int range = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            range += nums[i];
+        }
+        int sum = (range + target)/2;
+
+
+        return subsetSumPossible(nums, sum, n);
+    }
+
+    private static int subsetSumPossible(int[] nums, int sum, int n) {
+        int[][] dp = new int[n + 1][sum + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
